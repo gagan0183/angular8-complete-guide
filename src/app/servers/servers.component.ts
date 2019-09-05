0b48d83318bbf73cf7f33c8a3724b67e8d83e44e
Gagan <g.deepsingh1@gmail.com>
2019-09-05 13:53:58 +0530

    ngIf directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServers: boolean = false;
-  serverCreationStatus = 'No Server was created';
   serverName: any = '';
   userName: any = '';
+  serverCreated: boolean;
   constructor() {
     setTimeout(() => {
       this.allowNewServers = true;
@@ -17,7 +17,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = `Server was created! Server Name is ${this.serverName}`;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(e: any) {
